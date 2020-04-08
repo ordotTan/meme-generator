ef2c916e67020fcd07dafdd933e7ef4408bd16c8
Or Dotan <or.dotan@ridewithvia.com>
2020-04-08 11:36:13 +0300

    basic stuff working

--- js/memegen-controller.js
+++ js/memegen-controller.js
@@ -1,1 +1,68 @@
 'use strict'
+
+var gCanvas;
+var gCtx;
+
+function onInit() {
+    gCanvas = document.querySelector('#my-canvas');
+    // resizeCanvas()
+    gCtx = gCanvas.getContext('2d');
+    gCtx.fillStyle = 'lightblue'
+    gCtx.fillRect(0, 0, gCanvas.width, gCanvas.height)
+
+    renderGallery()
+}
+
+
+function resizeCanvas() {
+    var elContainer = document.querySelector('.canvas-container');
+    gCanvas.width = elContainer.offsetWidth
+    gCanvas.height = elContainer.offsetHeight
+}
+
+
+function onSelectImg(elImg, imgId) {
+    clearCanvas()
+    setMeme(imgId)
+    renderMeme(elImg, imgId)
+}
+
+function renderMeme(elImg) {
+    const meme = getMeme()
+    const lines = meme.lines
+    gCtx.drawImage(elImg, 0, 0, gCanvas.width, gCanvas.height);
+    lines.forEach(line => {
+        drawText(line.text)
+    });
+}
+
+function drawText(text, x = 50, y = 50) {
+    gCtx.font = '40px Ariel'
+    // gCtx.textAlign = 'center'
+    gCtx.fillText(text, x, y)
+    gCtx.strokeText(text, x, y)
+}
+
+function renderGallery() {
+    const images = getImages()
+    var strHtmls = images.map(getImgHTML)
+    document.querySelector('.images-content').innerHTML = strHtmls.join('')
+
+}
+
+function getImgHTML(image, idx) {
+    return `<img data-img-id="${idx + 1}" src="img/${idx + 1}.jpg" onclick="onSelectImg(this,${idx + 1})">`
+}
+
+
+function clearCanvas() {
+    gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height)
+    gCtx.fillRect(0, 0, gCanvas.width, gCanvas.height)
+}
+
+function onUpdateLine(elText) {
+    updateMemeLine(elText.value)
+    const meme = getMeme()
+    const elImg = document.querySelector(`[data-img-id="${meme.selectedImgID}"]`)
+    renderMeme(elImg)
+}
--- js/memegen-service.js
+++ js/memegen-service.js
@@ -1,1 +1,47 @@
 'use strict'
+
+var gKeywords = {
+    'happy': 12,
+    'sad': 3,
+    'funny': 8
+}
+
+var gImgs = [
+    { id: 1, url: 'img/1.jpg', keywords: ['happy'] },
+    { id: 2, url: 'img/2.jpg', keywords: ['sad'] },
+    { id: 3, url: 'img/3.jpg', keywords: ['funny'] }
+]
+
+var gMeme = {
+    selectedImgID:1,
+    selectedLineIdx:0,
+    lines: [
+        {
+            lineID: 1,
+            text:'bla',
+            align:'c',
+            color:'black'
+        }
+    ]
+}
+
+function getImages() {
+    return gImgs
+}
+
+function setMeme(imgId) {
+    gMeme.selectedImgID=imgId
+}
+
+function getMeme() {
+    return gMeme
+}
+
+function getMemeLines() {
+    return gMeme.lines
+}
+
+
+function updateMemeLine(text) {
+    gMeme.lines[gMeme.selectedLineIdx].text = text
+}
